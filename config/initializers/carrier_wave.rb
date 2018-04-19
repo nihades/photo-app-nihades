801a02a74aae0da6433d2217c07f65f04261126b
nihades <nihaades@gmail.com>
2018-04-19 15:39:54 +0000

    Adding AWS S3 region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,9 +6,11 @@ if Rails.env.production?
 
       :provider => 'AWS',
       
-      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
       
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
+      
+      :region                 => ENV['S3_REGION']
       
     }
 
